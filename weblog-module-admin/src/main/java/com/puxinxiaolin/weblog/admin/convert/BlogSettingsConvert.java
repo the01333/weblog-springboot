4aafe2eb05a3e4923b50b75a14869ee78ddbf2d8
the01333 <3149696140@qq.com>
2025-01-24 15:31:46 +0800

    feat: 引入 mapstruct

---
+++ weblog-module-admin/src/main/java/com/puxinxiaolin/weblog/admin/convert/BlogSettingsConvert.java
@@ -0,0 +1,24 @@
+package com.puxinxiaolin.weblog.admin.convert;
+
+import com.puxinxiaolin.weblog.admin.model.vo.blogSettings.UpdateBlogSettingsRequestVO;
+import com.puxinxiaolin.weblog.common.domain.dos.BlogSettingsDO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface BlogSettingsConvert {
+
+    /**
+     * 初始化 convert 实例
+     */
+    BlogSettingsConvert INSTANCE = Mappers.getMapper(BlogSettingsConvert.class);
+
+    /**
+     * UpdateBlogSettingsRequestVO -> BlogSettingsDO
+     *
+     * @param updateBlogSettingsRequestVO
+     * @return
+     */
+    BlogSettingsDO convertUpdateBlogSettingsRequestVOToBlogSettingsDO(UpdateBlogSettingsRequestVO updateBlogSettingsRequestVO);
+
+}
--- weblog-module-admin/src/main/java/com/puxinxiaolin/weblog/admin/service/impl/AdminBlogSettingsServiceImpl.java
+++ weblog-module-admin/src/main/java/com/puxinxiaolin/weblog/admin/service/impl/AdminBlogSettingsServiceImpl.java
@@ -1,6 +1,7 @@
 package com.puxinxiaolin.weblog.admin.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.puxinxiaolin.weblog.admin.convert.BlogSettingsConvert;
 import com.puxinxiaolin.weblog.admin.model.vo.blogSettings.UpdateBlogSettingsRequestVO;
 import com.puxinxiaolin.weblog.admin.service.AdminBlogSettingsService;
 import com.puxinxiaolin.weblog.common.domain.dos.BlogSettingsDO;
@@ -20,18 +21,9 @@ public class AdminBlogSettingsServiceImpl extends ServiceImpl<BlogSettingsMapper
     @Override
     public Response updateBlogSettings(UpdateBlogSettingsRequestVO updateBlogSettingsRequestVO) {
         // VO -> DO
-        BlogSettingsDO blogSettingsDO = BlogSettingsDO.builder()
-                .id(1L)
-                .logo(updateBlogSettingsRequestVO.getLogo())
-                .name(updateBlogSettingsRequestVO.getName())
-                .author(updateBlogSettingsRequestVO.getAuthor())
-                .introduction(updateBlogSettingsRequestVO.getIntroduction())
-                .avatar(updateBlogSettingsRequestVO.getAvatar())
-                .githubHomepage(updateBlogSettingsRequestVO.getGithubHomepage())
-                .giteeHomepage(updateBlogSettingsRequestVO.getGiteeHomepage())
-                .csdnHomepage(updateBlogSettingsRequestVO.getCsdnHomepage())
-                .zhihuHomepage(updateBlogSettingsRequestVO.getZhihuHomepage())
-                .build();
+        BlogSettingsDO blogSettingsDO = BlogSettingsConvert.INSTANCE
+                .convertUpdateBlogSettingsRequestVOToBlogSettingsDO(updateBlogSettingsRequestVO);
+        blogSettingsDO.setId(1L);
 
         // 保存或更新（当数据库中存在 ID 为 1 的记录时，则执行更新操作，否则执行插入操作）
         saveOrUpdate(blogSettingsDO);
